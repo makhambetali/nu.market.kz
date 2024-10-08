22cbed6c9ef3fea8042d02a0efb98d90a9b8c163
Yerassyl Orynbassar <orynbasarerasil@gmail.com>
2024-10-08 23:11:16 +0600

    home page design change

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-axettocg=3*#(g1kng@^5)*6mvy*kmgg_f+!q)%=bkb(=-n38^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.184', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ static/js/temp.js
@@ -0,0 +1,7 @@
+var obj = {
+    'logo':{
+        "ru":"Лого",
+        "en": "Logo"
+    }
+}
+obj['logo']['en']
